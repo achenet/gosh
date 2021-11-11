5a7ee335bc49ab4acc47f97fe1cd533dfdefe341
Ariel Chenet <apchenet@gmail.com>
2021-11-11 18:04:31 +0100

    First commit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/achenet/gosh
+
+go 1.16
---
+++ sh.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// Initialize env
+var env map[string]interface{}
+
+func main() {
+	fmt.Println("gosh!")
+	for {
+		// Read std input
+		r := bufio.NewReader(os.Stdin)
+		// ReadLine will NOT include the \n character at the end of the line.
+		l, _, err := r.ReadLine()
+		handle(err)
+		s := string(l)
+		fmt.Println(">", s)
+		fmt.Println(l)
+		// Process input
+
+		// Write std output
+
+		// Exit
+		if exit(s) {
+			break
+		}
+
+	}
+	fmt.Println("Bonne continuation!")
+}
+
+func handle(err error) {
+	if err != nil {
+		fmt.Println("")
+		os.Exit(420)
+	}
+}
+
+func exit(s string) bool {
+	if s == "exit" {
+		return true
+	}
+	return false
+}
